204041b635673d4e1f0a2e21090ac554464d6472
Scott Timme <scott.timme@gmail.com>
2021-04-16 12:24:42 -0500

    Initial "Hello World" stolen from Microsoft.com

--- TaxHelper/TaxHelper/MainPage.xaml.cs
+++ TaxHelper/TaxHelper/MainPage.xaml.cs
@@ -17,5 +17,12 @@ namespace TaxHelper
         {
             InitializeComponent();
         }
+
+        int count = 0;
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
     }
 }
